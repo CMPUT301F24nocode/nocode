ca9a8bd9d02ff58e6547c597aa427a83f3cc2c75
Aryan Singh <132884488+Aryaninfinity@users.noreply.github.com>
2024-09-26 18:58:00 -0600

    Create Triangle.java

---
+++ Triangle.java
@@ -0,0 +1,39 @@
+import java.awt.Shape;
+
+public abstract class Triangle implements Shape {
+    private double sideA;
+    private double sideB;
+    private double sideC;
+
+    public Triangle(double sideA, double sideB, double sideC) {
+        super();
+        this.sideA = sideA;
+        this.sideB = sideB;
+        this.sideC = sideC;
+    }
+
+    public double getSideA() {
+        return sideA;
+    }
+
+    public void setSideA(double sideA) {
+        this.sideA = sideA;
+    }
+
+    public double getSideB() {
+        return sideB;
+    }
+
+    public void setSideB(double sideB) {
+        this.sideB = sideB;
+    }
+
+    public double getSideC() {
+        return sideC;
+    }
+
+    public void setSideC(double sideC) {
+        this.sideC = sideC;
+    }
+
+}
